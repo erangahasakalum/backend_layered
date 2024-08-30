b9237f071055e8837231326c95559c85b8ad1a92
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-08-31 00:08:17 +0530

    divided into section

--- src/main/java/lk/ijse/gdse67/demo/HelloServlet.java
+++ src/main/java/lk/ijse/gdse67/demo/HelloServlet.java
@@ -1,27 +1,63 @@
 package lk.ijse.gdse67.demo;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import lk.ijse.gdse67.demo.dto.CustomerDTO;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+@WebServlet( value = "/demo")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
+    Connection connection;
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        try {
+            var cdx = new InitialContext();
+            DataSource pool = (DataSource) cdx.lookup("java:comp/env/jdbc/studentRegistration");//methana gana resourse eka onama ekak wenna puluwan eka api narrow cast krnawa dta source ekak wdiyata
+
+            this.connection = pool.getConnection();
+        } catch (NamingException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    static String SAVE_STUDENT ="INSERT INTO customer(id,name,city,telephone) VALUES(?,?,?,?)";
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!req.getContentType().toLowerCase().startsWith("application/json") || req.getContentType() == null) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
 
-    public void destroy() {
+        Jsonb jsonb = JsonbBuilder.create();
+        CustomerDTO studentDto = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(SAVE_STUDENT);
+            ps.setString(1,studentDto.getId());
+            ps.setString(2,studentDto.getName());
+            ps.setString(3,studentDto.getCity());
+            ps.setString(4,studentDto.getTelephone());
+
+            if (ps.executeUpdate()>0){
+                resp.getWriter().write("saved");
+            }else {
+                resp.getWriter().write("not");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/lk/ijse/gdse67/demo/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/gdse67/demo/dto/CustomerDTO.java
@@ -1,2 +1,17 @@
-package lk.ijse.gdse67.demo.dto;public class CustomerDTO {
+package lk.ijse.gdse67.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomerDTO {
+    private String id;
+    private String name;
+    private String city;
+    private String telephone;
 }
